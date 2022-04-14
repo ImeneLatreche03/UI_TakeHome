69f2e27ee159fe2a2bbe63d5ff233ed9c8b3235a
ImeneLatreche03 <103283654+ImeneLatreche03@users.noreply.github.com>
2022-04-14 00:45:39 -0500

    Update ProductPage.java

--- src/main/java/pageLayers/ProductPage.java
+++ src/main/java/pageLayers/ProductPage.java
@@ -6,10 +6,8 @@ import utils.ElementUtil;
 
 public class ProductPage {
 
-	public class ProductPage {
-
 	WebDriver driver;
-	ElementUtil util= new ElementUtil();
+	ElementUtil util = new ElementUtil();
 
 	public ProductPage(WebDriver driver) {
 		this.driver = driver;
@@ -19,45 +17,43 @@ public class ProductPage {
 
 	// By locators
 
-	private By kindlePaperWhite = By.xpath("//span[contains(text(),'More versions to consider')]");
-	private By paperWhite= By.xpath("(//a[@aria-label='Go to Kindle Paperwhite'])[1]");
-	private By targetToScroll=By.xpath("//span[@class='selection' and contains(text(),'Black')]");
+	private By kindlePaperWhite = By.xpath("//span[contains(text(),'Kindle Paperwhite') and @class='a-color-base']");
 	private By WithoutUnlimited = By.xpath("//button[@id='a-autoid-14-announce']");
 	private By withoutAds = By.xpath("//button[@id='a-autoid-16-announce']");
 	private By addToCart = By.xpath("//span[@id='submit.add-to-cart']");
 	private By submit = By.xpath("//span[@id='a-autoid-3']");
 
-	//Methods
-	
-	public void selectKindlePaperWhite() {
+	// Methods
 
-		WebElement target=driver.findElement(targetToScroll);
-		
-		util.scrollToElement(driver, target);
-		util.waitforElementToBeClickable(driver, kindlePaperWhite);
-		driver.findElement(kindlePaperWhite).click();
-		
-		util.waitforElementToBeClickable(driver,paperWhite);
-		util.clickWithJavaScript(driver, driver.findElement(paperWhite));
+	public void selectKindlePaperWhite() throws Exception {
+
+		Thread.sleep(5000);
+		WebElement kindlePaperWhite1 = driver.findElement(kindlePaperWhite);
+		util.waitforElementToBePresent(driver, kindlePaperWhite);
+		if (!(kindlePaperWhite1.isSelected())) {
+			kindlePaperWhite1.click();
+		}
+
 	}
 
-	public void selectWithoutKindleUnlimited() {
-
-		util.waitforElementToBeClickable(driver,WithoutUnlimited);
+	public void selectWithoutKindleUnlimited() throws Throwable {
+		util.waitforElementToBeClickable(driver, WithoutUnlimited);
 		driver.findElement(WithoutUnlimited).click();
 	}
 
 	public void selectWithoutAds() {
-
-		util.waitforElementToBeClickable(driver,withoutAds);
+		util.waitforElementToBeClickable(driver, withoutAds);
 		driver.findElement(withoutAds).click();
+
 	}
 
-	public void addToCart() {
+	public void addToCart() throws Exception {
+		Thread.sleep(3000);
 		driver.navigate().refresh();
-		util.waitforElementToBeClickable(driver,addToCart);
+		util.waitforElementToBeClickable(driver, addToCart);
 		driver.findElement(addToCart).click();
-		util.waitforElementToBeClickable(driver,submit);
+		util.waitforElementToBeClickable(driver, submit);
 		driver.findElement(submit).click();
+
 	}
 }
